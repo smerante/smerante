210b98489c9e239f4b4e176b9ddbec55ac33319c
Sammer95 <merante.s@hotmail.com>
2020-04-23 22:20:28 -0400

    fix: publish script step

--- projects/components/src/lib/components/button/button.component.spec.ts
+++ projects/components/src/lib/components/button/button.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ButtonComponent } from './button.component';
+import { SMButtonComponent } from './button.component';
 
 describe('ButtonComponent', () => {
-  let component: ButtonComponent;
-  let fixture: ComponentFixture<ButtonComponent>;
+  let component: SMButtonComponent;
+  let fixture: ComponentFixture<SMButtonComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ButtonComponent ]
+      declarations: [ SMButtonComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ButtonComponent);
+    fixture = TestBed.createComponent(SMButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
